1b0d61e0118de0d3aaf70796d7c1ae2d78e47a4f
Ivan Karl <delacruzivankarl@gmail.com>
2023-03-07 14:57:15 +0800

    Update Ass02_num03CruzDelaCruz .py

--- Ass02_num03CruzDelaCruz .py
+++ Ass02_num03CruzDelaCruz .py
@@ -1,11 +1,11 @@
 length=int(input("Enter list lenght: "))
 elements=input("Enter list elements: ")
-list=[int(item) for item in elements.split(' ')]
-list.sort()
+listNum=[int(item) for item in elements.split(' ')]
+listNum.sort()
 
 def mean() :
     total=0 
-    for item in list : 
+    for item in listNum : 
         total=total+int(item)
     return int(total/length)
 
@@ -13,12 +13,27 @@ def median() :
     if length % 2 == 0:
         left = int(length / 2) - 1
         right = left + 1
-        return int((list[left] + list[right])/2)
+        return int((listNum[left] + listNum[right])/2)
     else :
-        return int(list[int(length/2)])
+        return int(listNum[int(length/2)])
 
 def mode() :
-    
+    mode=""
+    counter={}
+    for item in listNum :
+        if item in counter : counter[item] += 1
+        else : counter[item] = 0
+    counter = dict(reversed(sorted(counter.items(), key=lambda item: item[1])))
+    listCounter = list(counter)
+    i = 1
+    mode = f"{listCounter[0]}"
+    while i < len(listCounter) :
+        if counter[listCounter[0]] == counter[listCounter[i]] :
+            mode += f", {listCounter[i]}"
+            i += 1
+        else : break
+    return mode
 
 print(f"MEAN: {mean()}")
 print(f"MEDIAN: {median()}")
+print(f"MODE: {mode()}")
